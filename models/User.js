7ce72f68dd9d2c7cfd1db0c82367d17711df178c
unknown <dev.oliinyk@gmail.com>
2023-06-17 22:10:12 +0300

    added signup and login functionality

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
 
 const UserSchema = new Schema(
   {
@@ -14,5 +16,35 @@ const UserSchema = new Schema(
   },
   { timestamps: true, collection: 'users' }
 );
+
+UserSchema.statics.signup = async function (email, password) {
+  if (!email || !password) throw Error('All fields required');
+
+  if (!validator.isEmail) throw Error('Invalid email');
+
+  if (!validator.isStrongPassword) throw Error('Password not strong');
+
+  const exists = await this.findOne({ email });
+  if (exists) throw Error('Email already registered');
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+UserSchema.statics.login = async function (email, password) {
+  if (!email || !password) throw Error('All fields required');
+
+  const user = await this.findOne({ email });
+  if (!user) throw Error('Email not registered');
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) throw Error('Incorrect password');
+
+  return user;
+};
 
 export default model('User', UserSchema);
