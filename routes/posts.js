1e3556e239de9afc43787beb858261fdbf1a9c38
unknown <dev.oliinyk@gmail.com>
2023-07-04 13:06:25 +0300

    added auth middleware

--- routes/posts.js
+++ routes/posts.js
@@ -6,13 +6,15 @@ import {
   deletePost,
   updatePost,
 } from '../controllers/postControllers.js';
+import { requireAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.use(requireAuth);
 router.get('/', getAllPosts);
 router.get('/:id', getPost);
 router.post('/', createPost);
 router.delete('/:id', deletePost);
-router.patch('/:id',updatePost);
+router.patch('/:id', updatePost);
 
 export default router;
